3b850267d66c51088266b9ff389d307a1987f413
Brad Leege <brad.leege@thetradedesk.com>
2022-10-12 13:37:57 -0500

    New OpenPassDevelopmentApp

---
+++ Development/OpenPassDevelopmentApp/OpenPassDevelopmentApp/OpenPassDevelopmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenPassDevelopmentAppApp.swift
+//  OpenPassDevelopmentApp
+//
+//  Created by Brad Leege on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenPassDevelopmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
