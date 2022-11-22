ebaaa2a78a22750ad93546f9e57c8cce5624d008
Brad Leege <brad.leege@thetradedesk.com>
2022-11-22 15:41:04 -0600

    Displaying all of UID2Token Data in RootView

--- Development/OpenPassDevelopmentApp/RootView.swift
+++ Development/OpenPassDevelopmentApp/RootView.swift
@@ -19,26 +19,52 @@ struct RootView: View {
                 .font(Font.system(size: 28, weight: .bold))
                 .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
             if viewModel.error != nil {
-                HStack {
+                List {
                     Text(LocalizedStringKey("root.label.error"))
                         .font(Font.system(size: 20, weight: .bold))
                         .foregroundColor(.red)
                     Text(String(viewModel.error?.localizedDescription ?? "Error"))
                         .font(Font.system(size: 16, weight: .regular))
-                }.padding()
+                }.listStyle(.plain)
             } else {
-                VStack {
-                    Text(LocalizedStringKey("root.label.uid2Token.advertising"))
-                        .font(Font.system(size: 20, weight: .bold))
-                    Text(String(viewModel.uid2Token?.advertisingToken ?? NSLocalizedString("common.nil", comment: "")))
-                        .font(Font.system(size: 16, weight: .regular))
-                }.padding()
-                VStack {
-                    Text(LocalizedStringKey("root.label.uid2Token.refresh"))
-                        .font(Font.system(size: 20, weight: .bold))
-                    Text(String(viewModel.uid2Token?.refreshToken ?? NSLocalizedString("common.nil", comment: "")))
-                        .font(Font.system(size: 16, weight: .regular))
-                }.padding()
+                List {
+                    Group {
+                        Text(LocalizedStringKey("root.label.uid2Token.advertisingToken"))
+                            .font(Font.system(size: 20, weight: .bold))
+                        Text(String(viewModel.uid2Token?.advertisingToken ?? NSLocalizedString("common.nil", comment: "")))
+                            .font(Font.system(size: 16, weight: .regular))
+                    }
+                    Group {
+                        Text(LocalizedStringKey("root.label.uid2Token.identityExpires"))
+                            .font(Font.system(size: 20, weight: .bold))
+                        Text(String(String(describing: viewModel.uid2Token?.identityExpires)))
+                            .font(Font.system(size: 16, weight: .regular))
+                    }
+                    Group {
+                        Text(LocalizedStringKey("root.label.uid2Token.refreshToken"))
+                            .font(Font.system(size: 20, weight: .bold))
+                        Text(String(viewModel.uid2Token?.refreshToken ?? NSLocalizedString("common.nil", comment: "")))
+                            .font(Font.system(size: 16, weight: .regular))
+                    }
+                    Group {
+                        Text(LocalizedStringKey("root.label.uid2Token.refreshFrom"))
+                            .font(Font.system(size: 20, weight: .bold))
+                        Text(String(describing: viewModel.uid2Token?.refreshFrom))
+                            .font(Font.system(size: 16, weight: .regular))
+                    }
+                    Group {
+                        Text(LocalizedStringKey("root.label.uid2Token.refreshExpires"))
+                            .font(Font.system(size: 20, weight: .bold))
+                        Text(String(describing: viewModel.uid2Token?.refreshExpires))
+                            .font(Font.system(size: 16, weight: .regular))
+                    }
+                    Group {
+                        Text(LocalizedStringKey("root.label.uid2Token.refreshResponseKey"))
+                            .font(Font.system(size: 20, weight: .bold))
+                        Text(String(viewModel.uid2Token?.refreshResponseKey ?? NSLocalizedString("common.nil", comment: "")))
+                            .font(Font.system(size: 16, weight: .regular))
+                    }
+                }.listStyle(.plain)
             }
             Button("Login With OpenPass") {
                 viewModel.startLoginFlow()
