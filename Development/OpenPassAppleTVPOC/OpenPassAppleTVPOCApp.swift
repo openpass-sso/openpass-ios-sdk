e4dfb4ccbc74ad7db7498332b41d0a95cdd470c0
Brad Leege <bleege@gmail.com>
2023-05-08 23:17:15 -0500

    Creating AppleTV POC Target

---
+++ Development/OpenPassAppleTVPOC/OpenPassAppleTVPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenPassAppleTVPOCApp.swift
+//  OpenPassAppleTVPOC
+//
+//  Created by Brad Leege on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenPassAppleTVPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
