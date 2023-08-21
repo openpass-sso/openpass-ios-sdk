bc50b0fbbbc723f5b459f004aa7089b4b9bd94b4
Brad Leege <brad.leege@thetradedesk.com>
2023-08-21 10:46:01 -0500

    Bump to Version 1.1.0 (#23)

--- Sources/OpenPass/OpenPassManager.swift
+++ Sources/OpenPass/OpenPassManager.swift
@@ -68,7 +68,7 @@ public final class OpenPassManager: NSObject {
     private let sdkName = "openpass-ios-sdk"
     
     /// The SDK version
-    public let sdkVersion = "1.0.0"
+    public let sdkVersion = "1.1.0"
     
     /// Keys and Values that need to be included in every network request
     private let baseRequestParameters: BaseRequestParameters
